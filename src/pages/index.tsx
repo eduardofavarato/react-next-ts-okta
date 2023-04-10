328c5c87ce2482b82bd379ee714c202ab483af43
Eduardo Favarato <eduardo.favarato@gmail.com>
2023-04-10 17:09:48 -0300

    Change Repositories to use Server Side Rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,9 +20,9 @@ export default function Home() {
 					</div>
 					<Link
 						className="px-4 py-1 text-sm text-slate-800 font-semibold rounded-full border border-slate-200 hover:text-white hover:bg-slate-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-offset-2"
-						href={"/repos"}
+						href={"/repositories"}
 					>
-						Repos
+						Repositories
 					</Link>
 				</div>
 			</div>
---
+++ src/pages/repositories/index.tsx
@@ -0,0 +1,38 @@
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Repository = {
+	name: string;
+};
+
+type RepositoriesProps = {
+	repositories: Repository[];
+};
+
+export default function Repositories({ repositories }: RepositoriesProps) {
+	return (
+		<div className={`p-4`}>
+			<h1 className="text-2xl lg:text-4xl text-center font-bold">Repos</h1>
+
+			<ul className="m-2 list-disc list-inside">
+				{repositories.map((repository) => (
+					<li key={repository.name}>{repository.name}</li>
+				))}
+			</ul>
+
+			<Link className="text-sm underline" href={"/"}>{`< Profile`}</Link>
+		</div>
+	);
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	const response = await fetch("https://api.github.com/users/eduardofavarato/repos");
+	const repositories: Repository[] = await response.json();
+
+	return {
+		props: {
+			repositories: repositories,
+		},
+	};
+};
