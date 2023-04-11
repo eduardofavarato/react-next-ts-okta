10a7bb09bcb131ed37b4e37406dd6adba047b48e
Eduardo Favarato <eduardo.favarato@gmail.com>
2023-04-11 12:44:15 -0300

    Add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  const allCookies = request.cookies.getAll()
+  console.log(allCookies)
+
+  return NextResponse.redirect(new URL('/', request.url))
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/test/:path*'],
+}
