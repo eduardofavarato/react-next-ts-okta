5a111d0e7b25109210ffe0826d2811adcc7ac2f2
Eduardo Favarato <eduardo.favarato@gmail.com>
2023-04-11 12:55:36 -0300

    Fix next image properties

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,12 @@ export default function Home() {
       <h1 className="text-2xl lg:text-4xl text-center font-bold">Profile</h1>
       <div className="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <Image
-          className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+          className="block mx-auto h-auto w-auto rounded-full sm:mx-0 sm:shrink-0"
           src="https://github.com/eduardofavarato.png"
           alt="Eduardo Favarato"
           width={100}
           height={24}
+          priority={true}
         ></Image>
         <div className="text-center space-y-2 sm:text-left">
           <div className="space-y-0.5 mb-2">
